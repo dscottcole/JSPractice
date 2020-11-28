c493fe673b21ac822a25c43bc55c85af7d7d7506
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-11-28 16:00:21 -0600

    finished tutorial

--- Practice.js
+++ Practice.js
@@ -1,7 +1,7 @@
 let first2 = [
     ["The first Thanksgiving was celebrated by Americans and what other group of people?", ["A) Samoans", "B) Pilgrims", "C) Incas", "D) Egyptians"] , "B"],
     ["How many continents are there?", ["A) 4", "B) 10", "C) 7", "D) too many to count"], "C" ], 
-    ["What is the force that cause objects to fall?", ["A) Gravity", "B) Wind", "C) The will of the gods", "D) Magic"], "A"]
+    ["What is the force that causes objects to fall?", ["A) Gravity", "B) Wind", "C) The will of the gods", "D) Magic"], "A"]
 ]
 
 let second2 = [
@@ -31,7 +31,7 @@ let askQuestion = () => {
         ${questions[grade][randomIndex][1][3]}`)
     
         if (answer.toUpperCase() !== questions[grade][randomIndex][2]) {
-            alert("You're an idiot")
+            alert("Incorrect answer.")
             break
         } else {
             alert(`You're right, ${name}. You are smarter than a ${grade + 1} grader!`)
